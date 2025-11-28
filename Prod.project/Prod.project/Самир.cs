dd291c1b6d9f79fe25c5de2a2851677bf4ada710
Samir <samirnad630@gmail.com>
2025-11-28 21:13:58 +0300

    Приветствую

---
+++ Prod.project/Prod.project/Самир.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prod.project
+{
+    internal class Самир
+    {
+
+        int i = 0;
+    }
+}
